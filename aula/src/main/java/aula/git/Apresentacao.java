8c31b75f51e355ddcc37d58b8cfcdce47ba376c0
Luan Santos <luankauan10@gmail.com>
2024-03-26 10:22:00 -0300

    colaborator add 10:20

--- aula/src/main/java/aula/git/Apresentacao.java
+++ aula/src/main/java/aula/git/Apresentacao.java
@@ -4,10 +4,15 @@ public class Apresentacao {
 
     public void apresentar() {
         this.lucasSouza();
+        this.luanSantos();
     }
 
     private void lucasSouza() {
         System.out.println("");
+        System.out.println("Estou colaborando com o git");
+    }
+
+    private void luanSantos() {
         System.out.println("Estou colaborando com o git");
     }
 }
